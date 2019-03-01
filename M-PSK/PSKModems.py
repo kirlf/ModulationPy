2ad0fddb6d1a71304f749499115fd0bc32939fa8
kirlf <33487079+kirlf@users.noreply.github.com>
2019-03-01 09:22:42 +0300

    Update PSKModems.py

--- M-PSK/PSKModems.py
+++ M-PSK/PSKModems.py
@@ -1,153 +1,249 @@
-#!/usr/bin/python
-
 import numpy as np
 import sys
+from time import time
+import random
+import matplotlib.pyplot as plt
 
-def fast_qpsk_mod(s):
-	m = (s[::2]*(-2)+1)*np.cos(np.pi/4)+1j*(s[1::2]*(-2)+1)*np.sin(np.pi/4)
-	return m
-
-def GRAYencoder(x):
-	for idx in range(len(x)):
-		if idx == 0:
-			y = x[idx]
-		else:
-			y = y + str ( int( x[idx] ) ^ int( x[idx-1] ) )
-	return y
-
-
-def PSKmodulator(modulation_order, phase_shift = np.pi/4, symbol_mapping = 'Gray', input_type = 'Binary'):
-	s = [0+i for i in range(modulation_order)]
-	m = list(np.exp(1j*phase_shift + 1j*2*np.pi*np.array(s)/modulation_order))
-	dict_out = {}
-	if input_type == 'Decimal':
-		if symbol_mapping == 'Binary':
-			for x, y in zip(s, m):
-				dict_out[x] = y
-		elif symbol_mapping == 'Gray':
-			s2 = []
-			for i in s:
-				symbol = bin(i)[2:]
-				if len(symbol) < np.log2(modulation_order):
-					symbol = int( (np.log2(modulation_order) - len(symbol)) )*'0'+symbol
-				s2.append( int(GRAYencoder(symbol), 2 ) )
-			for x, y in zip(s2, m):
-				dict_out[x] = y
-		else:
-			print("Wrong input data type (should be 'Gray' or 'Binary'). Now input_type = " + str(input_type))
-			sys.exit(0)
-	else:
-		if symbol_mapping == 'Binary':
-			b = []
-			for i in s:
-				a = bin(i)[2:]
-				if len(a) < np.log2(modulation_order):
-					a = int((np.log2(modulation_order) - len(a)))*'0'+a
-				if np.log2(modulation_order)%2 == 0:
-					a = a[::-1]
-				b.append(a)
-			for x, y in zip(b, m):
-				dict_out[x] = y
-		elif symbol_mapping == 'Gray':
-			s2 = []
-			for i in s:
-				symbol = bin(i)[2:]
-				if len(symbol) < np.log2(modulation_order):
-					symbol = int( (np.log2(modulation_order) - len(symbol)) )*'0'+symbol
-				s2.append( int(GRAYencoder(symbol), 2 ) )
-			b = []
-			for i in s2:
-				a = bin(i)[2:]
-				if len(a) < np.log2(modulation_order):
-					a = int((np.log2(modulation_order) - len(a)))*'0'+a
-				if np.log2(modulation_order)%2 == 0:
-					a = a[::-1]
-				b.append(a)
-			for x, y in zip(b, m):
-				dict_out[x] = y
-		else:
-			print("Wrong input data type (should be 'Gray' or 'Binary'). Now input_type = " + str(input_type))
-			sys.exit(0)		
-	return dict_out
-
-
-
-def modulate(x, modulation_order, code_book, binary_input = True):
-	modulated = []
-	if binary_input == True: 
-		m = []
-		n = int(np.log2(modulation_order))
-		length = len(x)
-		for c in range(int(length/n)):
-			s = ''
-			y = x[(c + (n - 1)*c):(((n - 1)*c) + (n - 1) + (1+c))]
-			for d in y:
-				s = s+str(int(d))
-			modulated.append(code_book[s])
-	elif binary_input == False:
-		for a in x:
-			modulated.append(code_book[x])
-	else:
-		print("Wrong data type for binary_input (should be True or False). Now input_type = " + str(input_type))		
-	return np.array(modulated)
-
-
-def PSKDemodulator(modulation_order, phase_shift = np.pi/4, symbol_mapping = 'Gray'):
-	zeros = []
-	ones = []
-	for c in range(int(np.log2(modulation_order))):
-		zeros.append([])
-		ones.append([])
-	codebook = PSKmodulator(modulation_order, phase_shift, symbol_mapping, 'Decimal')
-	s = [0+i for i in range(modulation_order)]
-	b = []
-	for i in s:
-		a = bin(i)[2:]
-		if len(a) < np.log2(modulation_order):
-			a = int((np.log2(modulation_order) - len(a)))*'0'+a
-		if np.log2(modulation_order)%2 == 0:
-			a = a[::-1]
-		b.append(a)
-	for idx, n in enumerate(b):
-		for ind, m in enumerate(n):
-			if m == '0':
-				zeros[ind].append(codebook[idx])
-			else:
-				ones[ind].append(codebook[idx])
-	return zeros, ones
-		
+class Modem:
+    def __init__(self, M, phi, SymMap='Gray', InType='Binary'):
+        self.M = M
+        self.phi = phi
+        if InType != 'Decimal' and InType != 'Binary':     
+            print("Wrong input data type (should be 'Decimal' or 'Binary').\n Now InType = " \
+                  + str(InType))
+            sys.exit(0)    
+        if SymMap != 'Gray' and SymMap != 'Binary':     
+            print("Wrong mapping type (should be 'Gray' or 'Binary').\n Now SymMap = " \
+                  + str(SymMap))
+            sys.exit(0)
+        self.SymMap = SymMap
+        self.InType = InType
+        if InType == 'Binary':
+            self.BinIn = True
+        else:
+            self.BinIn = False
+    
+    def gray_encoder(self, x):
+        for idx in range(len(x)):
+            if idx == 0:
+                y = x[idx]
+            else:
+                y = y + str ( int( x[idx] ) ^ int( x[idx-1] ) )
+        return y
+    
+    def __gray_encoding(self, s):
+        s2 = []
+        for i in s:
+            symbol = bin(i)[2:]
+            if len(symbol) < np.log2(self.M):
+                symbol = int( (np.log2(self.M) - len(symbol)) )*'0'+symbol
+            s2.append( int(self.gray_encoder(symbol), 2 ) )
+        return s2
+    
+    def __dict_make(self, s, m):
+        dict_out = {}
+        for x, y in zip(s, m):
+            dict_out[x] = y
+        return dict_out
+    
+    def de2bin(self, s):
+        b = []
+        for i in s:
+            a = bin(i)[2:]
+            if len(a) < np.log2(self.M):
+                a = int((np.log2(self.M) - len(a)))*'0'+a
+            if np.log2(self.M)%2 == 0:
+                a = a[::-1]
+            b.append(a)
+        return b
+    
+    def create_constellation(self, s, m, mode='Modulator'):
+        dict_out = {}
+        if mode == 'Modulator':
+            if self.InType == 'Decimal' and self.SymMap == 'Binary':
+                dict_out = self.__dict_make(s, m)
+            elif self.InType == 'Decimal' and self.SymMap == 'Gray':
+                s2 = self.__gray_encoding(s)
+                dict_out = self.__dict_make(s2, m)
+            elif self.InType == 'Binary' and self.SymMap == 'Binary':
+                b = self.de2bin(s)
+                dict_out = self.__dict_make(b, m)
+            elif self.InType == 'Binary' and self.SymMap == 'Gray':
+                s2 = self.__gray_encoding(s)
+                b = self.de2bin(s2)
+                dict_out = self.__dict_make(b, m)
+        elif mode == 'Demodulator':
+            if self.SymMap == 'Binary':
+                dict_out = self.__dict_make(s, m)
+            elif self.SymMap == 'Gray':
+                s2 = self.__gray_encoding(s)
+                dict_out = self.__dict_make(s2, m)
+        else:
+            print("Wrong mode (should be 'Modulator' or 'Demodulator').\n Now mode = " \
+                  + str(mode))
+            sys.exit(0)
+        return dict_out
+    
+    def ApproxLLR(self, x, zeros, ones, NoiseVar=1):
+        LLR = []
+        for d in range(len(zeros)): #or for d in range(len(ones)):
+            num = []
+            for z in zeros[d]:
+                num.append( list( ( ( np.real(x) - np.real(z) )**2 ) + ( (np.imag(x) - np.imag(z))**2 ) ) )
+            denum = []
+            for o in ones[d]:
+                denum.append( list( ( ( np.real(x) - np.real(o) )**2 ) + ( (np.imag(x) - np.imag(o))**2 ) ) )
+            num_post = np.amin(num, axis=0, keepdims=True)
+            denum_post = np.amin(denum, axis=0, keepdims=True)
+            llr = np.transpose(num_post[0]) - np.transpose(denum_post[0])
+            LLR.append(-llr/NoiseVar)
+        result = np.zeros((len(x)*len(zeros))) 
+        for i, n in enumerate(LLR):
+            result[0+i::len(zeros)] = n
+        return result
+    
+    def ExactLLR(self, x, zeros, ones, NoiseVar=1):
+        LLR = []
+        for d in range(len(zeros)): #or for d in range(len(ones)):
+            num = []
+            for z in zeros[d]:
+                num.append( list ( np.exp ( -1* ( ( ( np.real(x) - np.real(z) )**2 )\
+                                                 + ( (np.imag(x) - np.imag(z))**2 ) ) / NoiseVar ) ) )
+            denum = []
+            for o in ones[d]:
+                denum.append( list ( np.exp ( -1*  ( ( ( np.real(x) - np.real(o) )**2 )\
+                                                + ( (np.imag(x) - np.imag(o) )**2 ) ) / NoiseVar ) ) )
+            
+            num_post = np.sum(num, axis=0, keepdims=True)
+            denum_post = np.sum(denum, axis=0, keepdims=True)
+            llr = np.log(np.transpose(num_post[0]) - np.transpose(denum_post[0]))
+            LLR.append(llr)
+        result = np.zeros((len(x)*len(zeros))) 
+        for i, n in enumerate(LLR):
+            result[i::len(zeros)] = n
+        return result
+    
+    def plot_const(self):
+        const = self.create_constellation(self.s, self.m)
+        fig = plt.figure(figsize=(6, 4), dpi=150)
+        for i in list(const):
+            x = np.real(const[i])
+            y = np.imag(const[i])
+            plt.plot(x, y, 'o', color='green')
+            if x < 0:
+                h = 'right'
+                xadd = -.03
+            else:
+                h = 'left'
+                xadd = .03
+            if y < 0:
+                v = 'top'
+                yadd = -.03
+            else:
+                v = 'bottom'
+                yadd = .03
+            if (abs(x) < 1e-9 and abs(y) > 1e-9):
+                h = 'center'
+            elif abs(x) > 1e-9 and abs(y) < 1e-9:
+                v = 'center'     
+            plt.annotate(i,(x+xadd,y+yadd), ha=h, va=v)
+        if self.M == 2 and self.Type == 'PSK':
+            M = 'B'
+        elif self.M == 4 and self.Type == 'PSK':
+            M = 'Q'   
+        plt.grid()
+        plt.axvline(linewidth=1.0, color='black')
+        plt.axhline(linewidth=1.0, color='black')
+        plt.axis([-1.5,1.5,-1.5,1.5])
+        plt.title(str(M)+'-'+str(self.Type)+', phase rotation: '+str(round(self.phi, 5))+\
+                  ', Mapping: '+str(self.SymMap)+', Input: '+str(self.InType))
+        plt.show()
 	
-def ExactLLR(x, zeros, ones):
-	LLR = []
-	for c in range(len(x)):
-		for d in range(len(zeros)):
-			num = 0
-			for z in zeros[d]:
-				num =  num + ( np.exp ( -  ( ( ( np.real(x[c]) - np.real(z) )**2 ) + ( (np.imag(x[ c ]) - np.imag(z))**2 ) ) ) )
-			denum = 0
-			for o in ones[d]:
-				denum =  denum + ( np.exp ( -  ( ( ( np.real(x[c]) - np.real(o) )**2 ) + ( (np.imag(x[c]) - np.imag(o))**2 ) ) ) )
-			llr = np.log( num / denum )
-			LLR.append(llr) 
-	return LLR
+class PSKModulator(Modem):  
+    def __init__(self, M, phi, SymMap='Gray', InType='Binary'):
+        super().__init__(M, phi, SymMap, InType)
+        self.s = [i for i in range(self.M)]
+        self.m = list(np.exp(1j*self.phi + 1j*2*np.pi*np.array(self.s)/self.M))
+        self.Type = 'PSK'
 
+    
+    def __fast_qpsk_mod(self, s):
+        m = (s[::2]*(-2)+1)*np.cos(np.pi/4)+1j*(s[1::2]*(-2)+1)*np.sin(np.pi/4)
+        return m
+    
+    def init(self):
+        dict_out = self.create_constellation(self.s, self.m)
+        return dict_out
+        
+                                    
+    def modulate(self, x, code_book):
+        modulated = []
+        if self.M == 4 and self.phi == np.pi / 4:
+            if self.BinIn == True:
+                modulated = self.__fast_qpsk_mod(x) 
+            else:
+                s = self.de2bin(x)
+                modulated = self.__fast_qpsk_mod(s)
+        else:
+            if self.BinIn == True: 
+                m = []
+                n = int(np.log2(self.M))
+                length = len(x)
+                for c in range(int(length/n)):
+                    s = ''
+                    y = x[(c + (n - 1)*c):(((n - 1)*c) + (n - 1) + (1+c))]
+                    for d in y:
+                        s = s+str(int(d))
+                    modulated.append(code_book[s])
+            else:
+                for a in x:
+                    modulated.append(code_book[x])
+        return np.array(modulated)
 
-
-def ApproxLLR(x, zeros, ones):
-	LLR = []
-	for d in range(len(zeros)):
-		num = []
-		for z in zeros[d]:
-			num.append( list( ( ( np.real(x) - np.real(z) )**2 ) + ( (np.imag(x) - np.imag(z))**2 ) ) )
-		denum = []
-		for o in ones[d]:
-			denum.append( list( ( ( np.real(x) - np.real(o) )**2 ) + ( (np.imag(x) - np.imag(o))**2 ) ) )
-		num_post = np.amin(num, axis=0, keepdims=True)
-		denum_post = np.amin(denum, axis=0, keepdims=True)
-		llr = np.transpose(num_post[0]) - np.transpose(denum_post[0])
-		LLR.append(-llr)
-	result = np.zeros((len(x)*len(zeros))) 
-	for i, n in enumerate(LLR):
-		result[0+i::len(zeros)] = n
-	return result
-
+class PSKDemodulator(Modem):
+    def __init__(self, M, phi, SymMap='Gray', InType='Binary'):
+        super().__init__(M, phi, SymMap, InType)
+        
+    def __fast_qpsk_demod(self, x):
+        LLR = []
+        for inx in x:
+            re =  (-( np.real(inx) - np.cos(np.pi/4))**2 ) - ( -(np.real(inx) + np.cos(np.pi/4))**2 )
+            im =  (-( np.imag(inx) - np.sin(np.pi/4))**2 ) - ( -(np.imag(inx) + np.sin(np.pi/4))**2 )
+            LLR.append(float(re))
+            LLR.append(float(im))
+        return LLR
+    
+    
+    def init(self):
+        zeros = []
+        ones = []
+        for c in range(int(np.log2(self.M))):
+            zeros.append([])
+            ones.append([])
+        s = [i for i in range(self.M)]
+        m = list(np.exp(1j*self.phi + 1j*2*np.pi*np.array(s)/self.M))
+        codebook = self.create_constellation(s, m, mode='Demodulator')
+        b = self.de2bin(s)
+        for idx, n in enumerate(b):
+            for ind, m in enumerate(n):
+                if m == '0':
+                    zeros[ind].append(codebook[idx])
+                else:
+                    ones[ind].append(codebook[idx])
+        return zeros, ones
+    
+    def demodulate(self, x, zeros, ones, DecisionMethod):
+        if self.M == 4 and self.phi == np.pi / 4:
+            result = self.__fast_qpsk_demod(x)
+        else:
+            if DecisionMethod == 'Exact LLR':
+                result = self.ExactLLR(x, zeros, ones)
+            elif DecisionMethod == 'Approximate LLR':
+                result = self.ApproxLLR(x, zeros, ones)
+            elif DecisionMethod == 'Hard':
+                result = (np.sign(self.ApproxLLR(x, zeros, ones)) + 1) / 2
+            else:
+                print("Wrong Decision Method (should be Approximate LLR, Exact LLR or Hard). Now Decision Method = "\
+                      + str(DecisionMethod))
+                sys.exit(0)                            
+        return result  
