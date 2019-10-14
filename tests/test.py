5636b328db6765204a43ee303d2273fc2c504a48
Vladimir Fadeev <33487079+kirlf@users.noreply.github.com>
2019-10-14 15:44:01 +0300

    Create test.py

---
+++ tests/test.py
@@ -0,0 +1,45 @@
+from src.ModulationPy import *
+
+M = [4, 16, 64, 256, 1024]
+gm = [True, False]
+bn = [True, False]
+
+for m in M:
+    print(m)
+    for g in gm:
+        print('Gray mapping: '+str(g))
+        for b in bn:
+            print('Bin input/output: '+str(b))
+            if b == True:
+                mlp = int(np.log2(m))
+                sz = mlp*int(1e4)
+            else:
+                sz = int(1e5)
+            msg = np.random.randint(2, size=sz)
+            Modem = QAMModem(m, gray_map=g, bin_input=b, soft_decision = False, bin_output = b)
+            if np.array_equal(msg, Modem.demodulate(Modem.modulate(msg))) != True:
+                raise ValueError("Test failed")
+            else:
+                print('Passed.')
+
+M = [2, 4, 8, 16, 32]
+gm = [True, False]
+bn = [True, False]
+
+for m in M:
+    print(m)
+    for g in gm:
+        print('Gray mapping: '+str(g))
+        for b in bn:
+            print('Bin input/output: '+str(b))
+            if b == True:
+                mlp = int(np.log2(m))
+                sz = mlp*int(1e4)
+            else:
+                sz = int(1e5)
+            msg = np.random.randint(2, size=sz)
+            Modem = PSKModem(m, gray_map=g, bin_input=b, soft_decision = False, bin_output = b)
+            if np.array_equal(msg, Modem.demodulate(Modem.modulate(msg))) != True:
+                raise ValueError("Test failed")
+            else:
+                print('Passed.')
