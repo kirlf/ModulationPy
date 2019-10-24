de58a9340de2078c20de59e18204d8fd2308c2f4
Vladimir Fadeev <vladimir_fadeev1993@mail.ru>
2019-10-24 15:59:49 +0300

    Setup tools

---
+++ setup.py
@@ -0,0 +1,47 @@
+from setuptools import setup, find_packages
+
+DISTNAME = 'src'
+DESCRIPTION = 'Digital Linear Modems'
+LONG_DESCRIPTION = open('README.md').read()
+MAINTAINER = 'Vladimir Fadeev'
+MAINTAINER_EMAIL = 'vovenur@gmail.com'
+URL = 'https://github.com/kirlf/ModulationPy'
+LICENSE = 'BSD 3-Clause'
+VERSION = '0.1'
+
+
+setup(
+    name="ModulationPy",
+    version=VERSION,
+    packages=find_packages(),
+
+    # Project uses reStructuredText, so ensure that the docutils get
+    # installed or upgraded on the target machine
+        install_requires=[
+          'numpy>=1.7.1',
+          'matplotlib>=2.2.2',
+    ],
+
+    python_requires='>=3.6.4',
+	package_data = { DISTNAME : files },
+
+    # metadata to display on PyPI
+    license=LICENSE,
+    author=MAINTAINER,
+    author_email=MAINTAINER_EMAIL,
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    keywords="communications digital modulation demodulation psk qam",
+    url=URL,   # project home page, if any
+
+    classifiers=[
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python :: 3.5',
+        'Intended Audience :: Science/Research',
+        'Intended Audience :: Telecommunications Industry',
+        'Operating System :: Unix',
+        'Topic :: Scientific/Engineering',
+        'Topic :: Software Development',
+    ]
+
+)
