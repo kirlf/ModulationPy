5b5e8c738218b86f47bdf4139260df4383471c86
Vladimir Fadeev <vladimir_fadeev1993@mail.ru>
2019-10-28 11:11:47 +0300

    Docs

---
+++ doc/build/html/_sources/ModulationPy.txt
@@ -0,0 +1,2 @@
+.. automodule:: ModulationPy.ModulationPy
+    :members:
--- doc/build/html/searchindex.js
+++ doc/build/html/searchindex.js
@@ -1,1 +1,1 @@
-Search.setIndex({envversion:49,filenames:["index"],objects:{},objnames:{},objtypes:{},terms:{content:0,index:0,modul:0,page:0,search:0},titles:["Welcome to ModulationPy&#8217;s documentation!"],titleterms:{document:0,indice:0,modulationpi:0,tabl:0,welcom:0}})
+Search.setIndex({envversion:49,filenames:["ModulationPy","index"],objects:{},objnames:{},objtypes:{},terms:{content:1,index:1,modul:1,page:1,search:1},titles:["&lt;no title&gt;","Welcome to ModulationPy&#8217;s documentation!"],titleterms:{document:1,indice:1,modulationpi:1,tabl:1,welcom:1}})
