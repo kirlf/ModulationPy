85d1bbfb70e3772f5349e280614f464e9a6045c2
Vladimir Fadeev <vovenur@gmail.com>
2020-04-14 07:33:46 +0000

    Tests are corrected

--- tests.py
+++ tests.py
@@ -1,45 +1,46 @@
-from ModulationPy.ModulationPy import *
+import unittest
+import numpy as np
+from ModulationPy.ModulationPy import QAMModem, PSKModem
 
-M = [4, 16, 64, 256, 1024]
-gm = [True, False]
-bn = [True, False]
 
-for m in M:
-    print(m)
-    for g in gm:
-        print('Gray mapping: '+str(g))
-        for b in bn:
-            print('Bin input/output: '+str(b))
-            if b == True:
-                mlp = int(np.log2(m))
-                sz = mlp*int(1e4)
-            else:
-                sz = int(1e5)
-            msg = np.random.randint(2, size=sz)
-            Modem = QAMModem(m, gray_map=g, bin_input=b, soft_decision = False, bin_output = b)
-            if np.array_equal(msg, Modem.demodulate(Modem.modulate(msg))) != True:
-                raise ValueError("Test failed")
-            else:
-                print('Passed.')
+class TestThing(unittest.TestCase):
+  def test_QAMModem(self):
+    """ QAM modem  modulation and demodulation tests"""
+    M = [4, 16, 64] # modulation order
+    gm = [True, False] # Gray mapping
+    bn = [True, False] # binary input
 
-M = [2, 4, 8, 16, 32]
-gm = [True, False]
-bn = [True, False]
+    for m in M:
+        for g in gm:
+            for b in bn:
+                if b == True:
+                    mlp = int(np.log2(m))
+                    sz = mlp*int(1e4)
+                else:
+                    sz = int(1e5)
+                msg = np.random.randint(2, size=sz)
+                Modem = QAMModem(m, gray_map=g, bin_input=b, soft_decision = False, bin_output = b)
+                
+                np.testing.assert_array_equal(msg, Modem.demodulate(Modem.modulate(msg)))
 
-for m in M:
-    print(m)
-    for g in gm:
-        print('Gray mapping: '+str(g))
-        for b in bn:
-            print('Bin input/output: '+str(b))
-            if b == True:
-                mlp = int(np.log2(m))
-                sz = mlp*int(1e4)
-            else:
-                sz = int(1e5)
-            msg = np.random.randint(2, size=sz)
-            Modem = PSKModem(m, gray_map=g, bin_input=b, soft_decision = False, bin_output = b)
-            if np.array_equal(msg, Modem.demodulate(Modem.modulate(msg))) != True:
-                raise ValueError("Test failed")
-            else:
-                print('Passed.')
+  def test_PSKModem(self):
+    """ PSK modem  modulation and demodulation tests"""
+    M = [2, 4, 8, 16, 32] # modulation order
+    gm = [True, False] # Gray mapping
+    bn = [True, False] # binary input
+
+    for m in M:
+        for g in gm:
+            for b in bn:
+                if b == True:
+                    mlp = int(np.log2(m))
+                    sz = mlp*int(1e4)
+                else:
+                    sz = int(1e5)
+                msg = np.random.randint(2, size=sz)
+                Modem = PSKModem(m, gray_map=g, bin_input=b, soft_decision = False, bin_output = b)
+                np.testing.assert_array_equal(msg, Modem.demodulate(Modem.modulate(msg)))
+
+if __name__ == '__main__':
+    unittest.main()
+
